65f78042f43135701cf41ec9cfc6fd3f83dfde6b
kash <koangkangach05@gmail.com>
2026-09-08 15:30:20 +0200

    BrowserRouter used for navigating the webpages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/home.tsx'
 import Projects from './components/projects.tsx'
